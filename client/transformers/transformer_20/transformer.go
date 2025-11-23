aa5c7c53c7357dc6e92347091eeaf7dd182ea79b
Vivek Burman <vivekburman1997@gmail.com>
2025-11-23 17:27:41 +0530

    sADQWD

--- client/transformers/transformer_20/transformer.go
+++ client/transformers/transformer_20/transformer.go
@@ -3,8 +3,9 @@ package client_transformer_20
 import (
 	"etlfunnel/execution/models"
 )
+
 func Transformer(param *models.TransformerProps) (map[string]any, error) {
-	// code here, something is fishy here bor
+	// code here, let try again
 
 	return param.Record, nil
 }
